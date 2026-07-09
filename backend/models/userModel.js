f8219257c6699881e561aba5bcf971fa38dfa3e2
Jameel Robinson <jameel.robinson93@gmail.com>
2026-07-08 20:10:10 -0400

    Fix duplicate userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const db = require("../database/database");
 
 // Create a new user
 function createUser(firstName, lastName, email, password, role, callback) {
+
   const sql = `
     INSERT INTO users
     (first_name, last_name, email, password, role)
@@ -15,10 +16,12 @@ function createUser(firstName, lastName, email, password, role, callback) {
       callback(err, this);
     }
   );
+
 }
 
 // Find user by email
 function findUserByEmail(email, callback) {
+
   db.get(
     "SELECT * FROM users WHERE email = ?",
     [email],
@@ -26,40 +29,10 @@ function findUserByEmail(email, callback) {
       callback(err, row);
     }
   );
+
 }
 
 module.exports = {
   createUser,
-  findUserByEmail,
-};const db = require("../database/database");
-
-function createUser(firstName, lastName, email, password, role, callback) {
-  const sql = `
-    INSERT INTO users
-    (first_name, last_name, email, password, role)
-    VALUES (?, ?, ?, ?, ?)
-  `;
-
-  db.run(
-    sql,
-    [firstName, lastName, email, password, role],
-    function (err) {
-      callback(err, this);
-    }
-  );
-}
-
-function findUserByEmail(email, callback) {
-  db.get(
-    "SELECT * FROM users WHERE email = ?",
-    [email],
-    (err, row) => {
-      callback(err, row);
-    }
-  );
-}
-
-module.exports = {
-  createUser,
-  findUserByEmail,
+  findUserByEmail
 };
